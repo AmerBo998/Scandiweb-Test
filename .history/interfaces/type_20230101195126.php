180e2b7bf52d1e3f1013420aeefbb10bb84d60fe
Amer Bogilović <53022367+AmerBo998@users.noreply.github.com>
2023-01-01 19:57:01 +0100

    Delete .history directory

--- .history/interfaces/type_20230101195126.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//The 'Type' abstract class is created to help with getting product attributes 
-//during the 'insert-new-product' process.
-
-abstract class Type
-{
-    public abstract function getAttribute();
-}
